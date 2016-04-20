c425379de71eb06ec8298944aa70b2f366613053
firestorm451 <mzwick1684@hotmail.com>
2016-04-20 17:20:35 -0400

    Somewhat working front page

---
+++ app/controllers/stuff_controller.rb
@@ -0,0 +1,8 @@
+class StuffController < ApplicationController
+
+  def index
+    ruby_peeps = Octokit.search_users("location:Indianapolis language:Ruby", :sort => "joined", :order => :asc)[:items]
+    @users = ruby_peeps.map{|u| Octokit.user(u[:id])}
+  end
+
+end
